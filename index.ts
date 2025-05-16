7bbc467c98475a3b1a2e21d92bfe99aa8ab7131e
shay <43248357+shayypy@users.noreply.github.com>
2025-05-16 13:11:05 -0500

    feat: dst_until & dst_from, sort data, consolidate

--- index.ts
+++ index.ts
@@ -25,40 +25,74 @@ interface TimezoneResponse {
   week_number: number;
 }
 
+const getDstBounds = (
+  zone: moment.MomentZone,
+  now: moment.Moment,
+): Pick<TimezoneResponse, "dst_from" | "dst_until"> => {
+  const data: Pick<TimezoneResponse, "dst_from" | "dst_until"> = {
+    dst_from: null,
+    dst_until: null,
+  };
+  if (!now.isDST()) return data;
+
+  const unix = now.valueOf();
+  let protoFrom = zone.untils[0] ?? 0;
+  let protoUntil = zone.untils[zone.untils.length - 1] ?? 0;
+  for (const until of zone.untils) {
+    if (until < unix && until > protoFrom) {
+      protoFrom = until;
+    }
+    if (until > unix && until < protoUntil) {
+      protoUntil = until;
+    }
+  }
+
+  // return 0-offset timestamp strings
+  data.dst_from = moment(protoFrom).utc().format();
+  data.dst_until = moment(protoUntil).utc().format();
+  return data;
+};
+
+const getResponseForTimezone = (
+  zone: moment.MomentZone,
+  clientIp: string | null,
+): TimezoneResponse => {
+  // get current time in requested timezone
+  const now = moment.tz(zone.name);
+  const utcOffset = now.format("Z");
+  const dst = now.isDST();
+
+  // format response similar to worldtimeapi.org
+  return {
+    utc_offset: utcOffset,
+    timezone: zone.name, // correct capitalization
+    day_of_week: now.day(),
+    day_of_year: now.dayOfYear(),
+    datetime: now.format(),
+    utc_datetime: moment.utc().format(),
+    unixtime: Math.floor(now.valueOf() / 1000),
+    raw_offset: now.utcOffset() * 36,
+    week_number: now.week(),
+    dst,
+    abbreviation: now.zoneAbbr(),
+    dst_offset: dst ? 3600 : 0,
+    ...getDstBounds(zone, now),
+    client_ip: clientIp,
+  };
+};
+
 // get time by timezone in a given location (e.g. "/America/New_York")
 app.get("/api/timezone/:area/:location", (c) => {
   try {
     // check if timezone is valid
     const timezone = `${c.req.param("area")}/${c.req.param("location")}`;
-    if (!moment.tz.zone(timezone)) {
+    const zone = moment.tz.zone(timezone);
+    if (!zone) {
       return c.json({ error: "Timezone not found" }, 404);
     }
-
-    // get current time in requested timezone
-    const now = moment.tz(timezone);
-    const utcOffset = now.format("Z");
-    const dst = now.isDST();
-
-    // format response similar to worldtimeapi.org
-    const response = {
-      abbreviation: now.format("z"),
-      client_ip: c.req.header("CF-Connecting-IP") ?? null,
-      datetime: now.format(),
-      day_of_week: now.day(),
-      day_of_year: now.dayOfYear(),
-      dst,
-      dst_from: null,
-      dst_offset: dst ? 3600 : 0,
-      dst_until: null,
-      raw_offset: Number.parseInt(utcOffset.replace(":", "")) * 36,
-      timezone,
-      unixtime: Math.floor(now.valueOf() / 1000),
-      utc_datetime: moment.utc().format(),
-      utc_offset: utcOffset,
-      week_number: now.week(),
-    } satisfies TimezoneResponse;
-
-    return c.json(response);
+    return c.json(
+      getResponseForTimezone(zone, c.req.header("CF-Connecting-IP") ?? null),
+    );
   } catch (error) {
     console.error("Error processing timezone request:", error);
     return c.json({ error: "Failed to process timezone request" }, 500);
@@ -70,34 +104,13 @@ app.get("/api/timezone/:area", (c) => {
   try {
     // check if timezone is valid
     const timezone = c.req.param("area");
-    if (!moment.tz.zone(timezone)) {
+    const zone = moment.tz.zone(timezone);
+    if (!zone) {
       return c.json({ error: "Timezone not found" }, 404);
     }
-
-    // same logic as above, just with different parameter structure
-    const now = moment.tz(timezone);
-    const utcOffset = now.format("Z");
-    const dst = now.isDST();
-
-    const response = {
-      abbreviation: now.format("z"),
-      client_ip: c.req.header("CF-Connecting-IP") ?? null,
-      datetime: now.format(),
-      day_of_week: now.day(),
-      day_of_year: now.dayOfYear(),
-      dst,
-      dst_from: null,
-      dst_offset: dst ? 3600 : 0,
-      dst_until: null,
-      raw_offset: Number.parseInt(utcOffset.replace(":", "")) * 36,
-      timezone,
-      unixtime: Math.floor(now.valueOf() / 1000),
-      utc_datetime: moment.utc().format(),
-      utc_offset: utcOffset,
-      week_number: now.week(),
-    } satisfies TimezoneResponse;
-
-    return c.json(response);
+    return c.json(
+      getResponseForTimezone(zone, c.req.header("CF-Connecting-IP") ?? null),
+    );
   } catch (error) {
     console.error("Error processing timezone request:", error);
     return c.json({ error: "Failed to process timezone request" }, 500);
