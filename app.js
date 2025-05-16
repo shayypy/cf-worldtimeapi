51ab929fc071961dfc832720ab04ef40941ffb1f
shay <43248357+shayypy@users.noreply.github.com>
2025-05-16 13:20:47 -0500

    feat: `dst_from` and `dst_until`
    fixes #1

--- app.js
+++ app.js
@@ -7,13 +7,39 @@ const moment = require("moment-timezone");
 const app = express();
 const PORT = 1337;
 
+const getDstBounds = (zone, now) => {
+    const data = {
+        dst_from: null,
+        dst_until: null,
+    };
+    if (!now.isDST()) return data;
+
+    const unix = now.valueOf();
+    let protoFrom = zone.untils[0] ?? 0;
+    let protoUntil = zone.untils[zone.untils.length - 1] ?? 0;
+    for (const until of zone.untils) {
+        if (until < unix && until > protoFrom) {
+            protoFrom = until;
+        }
+        if (until > unix && until < protoUntil) {
+            protoUntil = until;
+        }
+    }
+
+    // return 0-offset timestamp strings
+    data.dst_from = moment(protoFrom).utc().format();
+    data.dst_until = moment(protoUntil).utc().format();
+    return data;
+}
+
 // Routes
 // get time by timezone in a given location (e.g. "/America/New_York")
 app.get("/api/timezone/:area/:location", (req, res) => {
     try {
         // check if timezone is valid
         const timezone = `${req.params.area}/${req.params.location}`;
-        if (!moment.tz.zone(timezone)) {
+        const zone = moment.tz.zone(timezone);
+        if (!zone) {
             return res.status(404).json({ error: "Timezone not found" });
         }
         
@@ -30,9 +56,8 @@ app.get("/api/timezone/:area/:location", (req, res) => {
             day_of_week: now.day(),
             day_of_year: now.dayOfYear(),
             dst,
-            dst_from: null,
             dst_offset: dst ? 3600 : 0,
-            dst_until: null,
+            ...getDstBounds(zone, now),
             raw_offset: parseInt(utcOffset.replace(":", "")) * 36,
             timezone,
             unixtime: Math.floor(now.valueOf() / 1000),
@@ -53,7 +78,8 @@ app.get("/api/timezone/:area", (req, res) => {
     try {
         // check if timezone is valid
         const timezone = req.params.area;
-        if (!moment.tz.zone(timezone)) {
+        const zone = moment.tz.zone(timezone);
+        if (!zone) {
             return res.status(404).json({ error: "Timezone not found" });
         }
         
@@ -69,9 +95,8 @@ app.get("/api/timezone/:area", (req, res) => {
             day_of_week: now.day(),
             day_of_year: now.dayOfYear(),
             dst,
-            dst_from: null,
             dst_offset: dst ? 3600 : 0,
-            dst_until: null,
+            ...getDstBounds(zone, now),
             raw_offset: parseInt(utcOffset.replace(":", "")) * 36,
             timezone,
             unixtime: Math.floor(now.valueOf() / 1000),
